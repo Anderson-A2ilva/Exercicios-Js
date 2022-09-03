9fbb387e63872b8a078afb78fa841bb768f833c6
Anderson2A <112587285+Anderson-A2ilva@users.noreply.github.com>
2022-09-03 15:21:16 -0300

    first commit

---
+++ Aula 11/js/index.js
@@ -0,0 +1,5 @@
+let num1 = prompt("Digite um número");
+num1 = parseFloat(num1); 
+let num2 = prompt("Digite um segundo número");
+num2 = parseFloat(num2);
+alert(`O resultado foi: ${num1 + num2}`);
---
+++ Funções/index.js
@@ -0,0 +1,4 @@
+function addition(number1, number2 = 2){
+    return number1 + number2;
+}
+console.log(addition(3,5));  
